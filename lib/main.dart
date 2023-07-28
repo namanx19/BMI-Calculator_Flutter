a292d1c691033c942019a50534267ca089f99223
Naman Gupta <naman.mw4@gmail.com>
2023-07-29 02:01:08 +0530

    init

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class bmiCalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData.dark().copyWith(
           appBarTheme: const AppBarTheme(
             color: Color(0xff0a0d22),
